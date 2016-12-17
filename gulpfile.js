a48dcc189886941189a5818cdd0ba749af28772e
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2016-12-17 12:43:12 +0100

    chore(test): run tests with gulp-jasmine

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
-require('require-dir')('build/tasks');
+var gulp = require("gulp");
+var jasmine = require("gulp-jasmine");
+
+gulp.task("test", () => {
+  gulp.src("dist/test/test/**/*.js")
+    .pipe(jasmine({
+      verbose: true,
+      includeStackTrace: true,
+      errorOnFail: false
+    }));
+});
+
